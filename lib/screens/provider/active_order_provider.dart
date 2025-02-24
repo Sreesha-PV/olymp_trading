b2e083a04f7e2e5d3d977f41a7674837f1eef734
Sreesha-PV <sreeshaaapv2001@gmail.com>
2025-02-24 12:44:20 +0400

    Add files via upload

---
+++ lib/screens/provider/active_order_provider.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/material.dart';
+import 'package:olymp_trade/model/order_model.dart';
+
+class ActiveOrderProvider with ChangeNotifier {
+  List<Order> _orders = [];
+
+  List<Order> get orders => _orders;
+
+  void addOrder(Order order) {
+    _orders.add(order);
+    notifyListeners();
+  }
+
+  void clearOrders() {
+    _orders.clear();
+    notifyListeners();
+  }
+}
+
+
+
+
