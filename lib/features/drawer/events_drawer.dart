7b119c626789b292927e22557c05efc1601f4a16
Sreesha-PV <sreeshaaapv2001@gmail.com>
2025-02-27 21:46:13 +0400

    Add files via upload

---
+++ lib/features/drawer/events_drawer.dart
@@ -0,0 +1,125 @@
+
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class EventsDrawer extends StatelessWidget {
+  const EventsDrawer({
+    super.key,
+  });
+  
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      // backgroundColor: const Color.fromARGB(246, 17, 17, 17),
+      backgroundColor: const Color.fromARGB(255, 24, 23, 23),
+
+      child: ListView(
+        padding: const EdgeInsets.only(left: 15),
+        children: [
+          Row(
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(top: 30, left: 12),
+                child: Text(
+                  'Events',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 26),
+                ),
+              ),
+              const SizedBox(width: 120),
+              const Icon(CupertinoIcons.clock),
+              IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: const Icon(Icons.close),
+                color: Colors.white,
+              ),
+            ],
+          ),
+          // Use SingleChildScrollView to handle scrolling content.
+          Padding(
+            padding: const EdgeInsets.only(top: 20, right: 12),
+            child: Container(
+              height: 260,
+              decoration: BoxDecoration(
+                  color: Colors.black, borderRadius: BorderRadius.circular(12)),
+              child: Padding(
+                padding: const EdgeInsets.all(12),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'HAPPENING NOW',
+                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+                      ),
+                      const SizedBox(height: 40),
+                      const Row(
+                        children: [
+                          Text(
+                            'Welcome to ',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20),
+                          ),
+                        ],
+                      ),
+                      const Row(
+                        children: [
+                          Text(
+                            'Olymptrade',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20),
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      const Column(
+                        children: [
+                          Text(
+                            'Ends on',
+                            style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          const Text(
+                            'May 1',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16),
+                          ),
+                          const SizedBox(
+                            width: 150,
+                          ),
+                          IconButton(
+                              onPressed: () {},
+                              icon: const Icon(Icons.arrow_right_alt))
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/features/drawer/trades_drawer.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:olymp_trade/features/drawer/sidebar_drawr/trades_pages/stock_page.dart';
+import 'trades_pages/fixed_page.dart';
+import 'trades_pages/forex_page.dart';
+
+class TradesDrawer extends StatelessWidget {
+  const TradesDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: const Color.fromARGB(255, 24, 23, 23),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 14, top: 15),
+            child: Row(
+              children: [
+                const Text(
+                  'Trades',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 26),
+                ),
+                const SizedBox(width: 150),
+                IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: const Icon(Icons.close),
+                  color: Colors.white,
+                )
+              ],
+            ),
+          ),
+          const Flexible(  
+            child: DefaultTabController(
+              length: 3,
+              child: Column(
+                children: [
+                  TabBar(
+                    unselectedLabelColor: Colors.grey,
+                    labelColor: Colors.white,
+                    dividerColor: Colors.grey,
+                    indicatorColor: Colors.white,
+                    tabs: [
+                      Tab(text: 'Fixed Time'),
+                      Tab(text: 'Forex'),
+                      Tab(text: 'Stocks'),
+                    ],
+                  ),
+                  Flexible( 
+                    child: TabBarView(
+                      children: [
+                        FixedTimePage(),
+                        ForexPage(),
+                        StocksPage(),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+                       
+
+                       
