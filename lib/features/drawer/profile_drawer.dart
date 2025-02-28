e7a0431169c60b81429b59bc024ea407457ac0e5
Sreesha-PV <sreeshaaapv2001@gmail.com>
2025-02-28 17:30:59 +0400

    Update profile_drawer.dart

--- lib/features/drawer/profile_drawer.dart
+++ lib/features/drawer/profile_drawer.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:olymp_trade/features/authentication/authentication_screen.dart';
 
 class ProfileDrawer extends StatelessWidget {
   final int selectedIndex;
   final Function(int) onSelect;
+  final String email; 
 
   const ProfileDrawer({
     super.key,
     required this.selectedIndex,
     required this.onSelect,
+    required this.email, 
   });
 
   @override
@@ -21,7 +23,6 @@ class ProfileDrawer extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-         
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -41,24 +42,40 @@ class ProfileDrawer extends StatelessWidget {
             ],
           ),
           
-     
           const SizedBox(height: 50), 
           CircleAvatar(
             radius: 40, 
             backgroundColor: Colors.grey[850], 
             child: const Icon(CupertinoIcons.person,
-            color: Colors.white,
-            size: 30,),
+              color: Colors.white,
+              size: 30,
+            ),
           ),
-
         
           const SizedBox(height: 8),
-          const Text(
-            "User",
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-            fontWeight: FontWeight.bold),),
+          Text(
+            email,  
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+              fontWeight: FontWeight.bold
+            ),
+            textAlign: TextAlign.center,
+          ),
+
+          const SizedBox(height: 15,),
+          Container(
+            height: 50,
+            width: 100,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: Colors.grey[800],
+            ),
+            child: TextButton(onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=>AuthScreen()));
+            }, child: const Text('Logout',
+            style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 14),)),
+          )
         ],
       ),
     );
