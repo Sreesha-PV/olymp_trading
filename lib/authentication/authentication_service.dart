b1e91609f554144783314553bb6da2005e390f6a
Sreesha-PV <sreeshaaapv2001@gmail.com>
2025-02-28 13:07:08 +0400

    Update authentication_service.dart

--- lib/authentication/authentication_service.dart
+++ lib/authentication/authentication_service.dart
@@ -1,29 +1,48 @@
-// import 'package:flutter/material.dart';
 
-// class AuthService extends ChangeNotifier {
-//   // In-memory user storage (for demo purposes)
-//   final Map<String, String> _users = {};
+
+
+
+import 'package:flutter/material.dart';
+
+class AuthProvider with ChangeNotifier {
+  String? registeredEmail;
+  String? registeredPassword;
+  bool isLoggedIn = false;
+  bool rememberMe = false;
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+   final formKey = GlobalKey<FormState>();
+
+  // Register method
+  void register(String email, String password) {
+    if (email.isNotEmpty && password.isNotEmpty) {
+      registeredEmail = email;
+      registeredPassword = password;
+      emailController.clear();
+      passwordController.clear();
+      isLoggedIn = true;
+      notifyListeners();
+    } else {
+      throw Exception("Please enter both email and password.");
+    }
+  }
+
+ 
+  void login(String email, String password) {
+    if (email.isEmpty || password.isEmpty) {
+      throw Exception("Please enter both email and password.");
+    } else if (registeredEmail == email && registeredPassword == password) {
+      isLoggedIn = true;
+      notifyListeners();
+    } else {
+      throw Exception("Invalid credentials. Please register first.");
+    }
+  }
 
   
-//   void register(String username, String password) {
-//     if (_users.containsKey(username)) {
-//       // User already exists
-//       throw Exception("User already exists!");
-//     }
-//     _users[username] = password;
-//     notifyListeners();
-//   }
-
-  
-//   bool login(String username, String password) {
-    
-//     if (_users.containsKey(username) && _users[username] == password) {
-//       return true;  
-//     }
-//     return false;  
-//   }
-
-//   bool userExists(String username) {
-//     return _users.containsKey(username);
-//   }
-// }
+  void toggleRememberMe(bool value) {
+    rememberMe = value;
+    notifyListeners();
+  }
+}
