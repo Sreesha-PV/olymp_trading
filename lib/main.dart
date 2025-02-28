76ba4593ae64483f05230c172c127b53fd989e0b
Sreesha-PV <sreeshaaapv2001@gmail.com>
2025-02-28 11:35:03 +0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:olymp_trade/features/authentication/authentication_screen.dart';
 import 'package:olymp_trade/features/authentication/authentication_service.dart';
 import 'package:olymp_trade/features/authentication/login.dart';
 import 'package:olymp_trade/features/authentication/registration.dart';
-import 'package:olymp_trade/features/provider/accountdrawer_provider.dart';
+import 'package:olymp_trade/features/provider/account_drawer_provider.dart';
 import 'package:olymp_trade/features/provider/active_order_provider.dart';
 import 'package:olymp_trade/features/provider/drawer_provider.dart';
 import 'package:olymp_trade/features/provider/dropdown_provider.dart';
 import 'package:olymp_trade/features/provider/enable_order_provider.dart';
+import 'package:olymp_trade/features/provider/icon_provider.dart';
 import 'package:olymp_trade/features/provider/order_provider.dart';
 import 'package:olymp_trade/features/provider/selected_account_provider.dart';
 import 'package:olymp_trade/features/provider/selected_index_provider.dart';
 import 'package:olymp_trade/features/provider/tabbar_provider.dart';
-import 'package:olymp_trade/features/provider/trade_settings_provider.dart';
 import 'package:olymp_trade/features/provider/transaction_history_provider.dart';
 import 'package:olymp_trade/features/trading/trade_screen.dart';
 import 'package:provider/provider.dart';
@@ -41,7 +42,7 @@ class MyApp extends StatelessWidget {
       ChangeNotifierProvider(create: (_) => ActiveOrderProvider()),
       ChangeNotifierProvider(create: (_) => EnableOrderProvider()),
       ChangeNotifierProvider(create: (_) => ProfitabilityProvider()),
-      ChangeNotifierProvider(create: (_)=>AuthService()),
+      // ChangeNotifierProvider(create: (_)=>AuthService()),
       ChangeNotifierProvider(create: (_)=>TransactionHistoryProvider()),
       ChangeNotifierProvider(create: (_) => AccountProvider()),
       ChangeNotifierProvider(create: (_) => SelectedAccountNotifier()),
@@ -54,9 +55,9 @@ class MyApp extends StatelessWidget {
         title: 'Flutter Demo',
         debugShowCheckedModeBanner: false,
          theme: ThemeData.dark(),
+           home: AuthScreen(),
             // home: TradeScreen(), 
-            home: RegistrationScreen(),    
-          
+           
       ),
     );
   }
