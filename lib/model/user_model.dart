c089d0184585f9c962d41ed6be3c8b27a964eaa0
Sreesha-PV <sreeshaaapv2001@gmail.com>
2025-03-18 11:04:01 +0400

    Update user_model.dart

--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -1,6 +1,28 @@
-class User {
-  final String username;
-  final String password;
+class UserData {
+  int? userId;
+  String? userUuid;
+  String? email;
+  String? name;
 
-  User({required this.username, required this.password});
+  UserData({this.userId, this.userUuid, this.email, this.name});
+
+
+  UserData.fromJson(Map<String, dynamic> json) {
+    var data = json['data']; 
+    userId = data['user_id']; 
+    userUuid = data['user_uuid']; 
+    email = data['email']; 
+    name = data['name']; 
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['user_id'] = userId;
+    data['user_uuid'] = userUuid;
+    data['email'] = email;
+    data['name'] = name;
+
+    return data;
+  }
 }
+
