0667a6ccdf7a9d843f28ef7d38d5faa6845cf442
Sreesha-PV <sreeshaaapv2001@gmail.com>
2025-03-18 11:11:34 +0400

    Update active_order_provider.dart

--- lib/features/provider/active_order_provider.dart
+++ lib/features/provider/active_order_provider.dart
@@ -1,22 +1,45 @@
 
+
 import 'package:flutter/material.dart';
-import 'package:olymp_trade/features/model/order_model.dart';
+import 'package:olymp_trade/features/model/order_get_model.dart';
+
+import 'package:olymp_trade/services/order_get_services.dart';
 
 
-class ActiveOrderProvider with ChangeNotifier {
-  List<Order> _orders = [];
 
-  List<Order> get orders => _orders;
+const int activeOrderStatus =2;
+const int executedOrderStatus = 4;
 
-  void addOrder(Order order) {
-    _orders.add(order);
+
+class ActiveOrderProvider extends ChangeNotifier {
+  List<OrderGet> _orders = [];
+  bool _isLoading = false;
+  String? _errorMessage;
+
+
+  List<OrderGet> get orders => _orders;
+  bool get isLoading => _isLoading;
+  String? get errorMessage => _errorMessage;
+
+  Future<void> fetchOrders() async {
+    _isLoading = true;
+    _errorMessage = null;
     notifyListeners();
-  }
 
-  void clearOrders() {
-    _orders.clear();
-    notifyListeners();
+    try {
+      _orders = await OrderGetServices().getOrdersByStatus(activeOrderStatus);
+    } catch (e) {
+      _errorMessage = e.toString();
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
   }
+ 
+
 }
+
+
+
 
 
