ea5812c02d330326f9fb082be981f66f50328a29
Sreesha-PV <sreeshaaapv2001@gmail.com>
2025-03-21 15:43:32 +0400

    Update trade_history_provider.dart

--- lib/features/provider/trade_history_provider.dart
+++ lib/features/provider/trade_history_provider.dart
@@ -1,83 +1,9 @@
-// import 'package:flutter/material.dart';
-// import 'package:olymp_trade/features/model/transaction_model.dart';
-// import 'package:olymp_trade/services/transaction_history_services.dart';
-
-// class TransactionHistoryProvider with ChangeNotifier{
-//   final ApiService _apiService = ApiService();
-
-//   // Fetch transactions from the API
-//   Future<List<Transaction>> fetchTransactions() async {
-//     try {
-//       return await _apiService.fetchTransactions();
-//     } catch (error) {
-//       throw Exception('Failed to load transaction history');
-//     }
-//   }
-// }
-
-
-// import 'package:flutter/foundation.dart';
-// import 'package:olymp_trade/features/model/trade_history_model.dart';
-// import 'package:olymp_trade/services/trade_history_services.dart';
-
-// class TradeHistoryProvider extends ChangeNotifier{
-
-//   final TradeHistoryServices _tradeHistoryService= TradeHistoryServices();
-//   bool _isLoading = false;
-//   bool get isLoading => _isLoading;
-
-//   String? _errorMessage;
-//   String? get errorMessage => _errorMessage;
-
-//   Future<List<TradeHistory>>getTradeHistory()async{
-//    try {
-//       return await _tradeHistoryService.getTradeHistory();
-//     } catch (error) {
-//       throw Exception('Failed to load transaction history');
-//     }
-//   }
-// }
-
-
-
-// import 'package:flutter/material.dart';
-// import 'package:olymp_trade/features/model/trade_history_model.dart';
-// import 'package:olymp_trade/services/trade_history_services.dart';
-
-
-// class TradeHistoryProvider with ChangeNotifier {
-//   List<TradeHistory> _tradeHistory = [];
-//   bool _isLoading = false;
-//   String _errorMessage = '';
-
-//   List<TradeHistory> get tradeHistory => _tradeHistory;
-//   bool get isLoading => _isLoading;
-//   String get errorMessage => _errorMessage;
-
-//   Future<void> fetchTradeHistory() async {
-//     _isLoading = true;
-//     notifyListeners(); 
-
-//     try {
-//       TradeHistoryServices tradeHistoryServices = TradeHistoryServices();
-//       _tradeHistory = await tradeHistoryServices.getTradeHistory();
-//       _isLoading = false;
-//     } catch (e) {
-//       _errorMessage = e.toString();
-//       _isLoading = false;
-//     }
-
-//     notifyListeners(); 
-//   }
-// }
-
-
-
 
 
 import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:olymp_trade/features/model/order_get_model.dart';
 import 'package:olymp_trade/features/model/trade_history_model.dart';
 import 'package:olymp_trade/services/trade_history_services.dart';
 
@@ -86,7 +12,7 @@ class TradeHistoryProvider extends ChangeNotifier {
   List<TradeHistory> _tradeHistory = [];
   bool _isLoading = false;
   String? _errorMessage;
-  late final StreamSubscription _socketSubscription;
+  // late final StreamSubscription _socketSubscription;
 
   List<TradeHistory> get tradeHistory => _tradeHistory;
   bool get isLoading => _isLoading;
@@ -101,7 +27,6 @@ class TradeHistoryProvider extends ChangeNotifier {
     _isLoading = true;
     _errorMessage = null;
     notifyListeners();
-
     try {
       _tradeHistory = await _service.fetchTrades();
     } catch (e) {
@@ -110,6 +35,17 @@ class TradeHistoryProvider extends ChangeNotifier {
       _isLoading = false;
       notifyListeners();
     }
+  }
+
+    void addTrade(OrderGet order) {
+    final trade = TradeHistory(
+      symbol: order.symbol,
+      amount: order.amount,
+      timestamp: DateTime.now().millisecondsSinceEpoch,
+  
+    );
+    _tradeHistory.add(trade);
+    notifyListeners();
   }
 
   // void _connectWebSocket() {
