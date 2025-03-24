bf3b13e55698bcacff5582b66415f113471c2064
Sreesha-PV <sreeshaaapv2001@gmail.com>
2025-03-24 14:04:51 +0400

    Update selected_account_provider.dart

--- lib/features/provider/selected_account_provider.dart
+++ lib/features/provider/selected_account_provider.dart
@@ -1,25 +1,66 @@
+// import 'package:flutter/material.dart';
+// import 'package:olymp_trade/services/user_balance.dart';
+
+
+// class SelectedAccountNotifier extends ChangeNotifier {
+//   bool _isOpen = false;
+//   String _selectedAccount = "AED Account";
+//   Map<String, String> _balances = {
+//     "Demo Account": "Ð10,000.00",
+//     "AED Account": "AED 0.00",
+//     "USDT Account": "USDT 0.00",
+//   };
+
+//   bool get isOpen => _isOpen;
+//   String get selectedAccount => _selectedAccount;
+//   String get selectedBalance => _balances[_selectedAccount] ?? "0.00"; 
+
+//   SelectedAccountNotifier() {
+//     fetchBalanceFromAPI();
+//   }
+
+//   void toggleDrawer() {
+//     _isOpen = !_isOpen;
+//     notifyListeners();
+//   }
+//   void selectAccount(String account) {
+//     _selectedAccount = account;
+//     _isOpen = false;
+//     notifyListeners();
+//   }
+
+//   Future<void> fetchBalanceFromAPI() async {
+//     _balances = await BalanceService.fetchBalances();
+//     notifyListeners();
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
 import 'package:flutter/material.dart';
-import 'package:olymp_trade/services/user_balance.dart';
+import 'package:olymp_trade/services/account_balance_services.dart';
 
 
 class SelectedAccountNotifier extends ChangeNotifier {
+  String _selectedAccount = '';
+  String _selectedBalance = '0.00';
   bool _isOpen = false;
-  String _selectedAccount = "AED Account";
-  Map<String, String> _balances = {
-    "Demo Account": "Ð10,000.00",
-    "AED Account": "AED 0.00",
-    "USDT Account": "USDT 0.00",
-  };
 
-  bool get isOpen => _isOpen;
   String get selectedAccount => _selectedAccount;
-  String get selectedBalance => _balances[_selectedAccount] ?? "0.00"; 
+  String get selectedBalance => _selectedBalance;
+  bool get isOpen => _isOpen;
 
-  SelectedAccountNotifier() {
-    fetchBalanceFromAPI();
-  }
-
-  void toggleDrawer() {
+   void toggleDrawer() {
     _isOpen = !_isOpen;
     notifyListeners();
   }
@@ -29,9 +70,8 @@ class SelectedAccountNotifier extends ChangeNotifier {
     notifyListeners();
   }
 
-  Future<void> fetchBalanceFromAPI() async {
-    _balances = await BalanceService.fetchBalances();
-    notifyListeners();
+  SelectedAccountNotifier() {
+    BalanceService();
   }
 }
 
