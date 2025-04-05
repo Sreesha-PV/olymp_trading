ce02dd00ae526e0c7f33736adc1103d11a93b889
Sreesha-PV <sreeshaaapv2001@gmail.com>
2025-04-05 13:54:49 +0400

    Update trade_history_provider.dart

--- lib/features/provider/trade_history_provider.dart
+++ lib/features/provider/trade_history_provider.dart
@@ -1,83 +1,130 @@
 
 
+// import 'dart:async';
+// import 'dart:convert';
+// import 'package:flutter/material.dart';
+// import 'package:olymp_trade/features/model/order_get_model.dart';
+// import 'package:olymp_trade/features/model/trade_history_model.dart';
+// import 'package:olymp_trade/services/trade_history_services.dart';
+
+// class TradeHistoryProvider extends ChangeNotifier {
+//   final TradeHistoryServices _service = TradeHistoryServices();
+//   List<TradeHistory> _tradeHistory = [];
+//   bool _isLoading = false;
+//   String? _errorMessage;
+//   // late final StreamSubscription _socketSubscription;
+
+//   List<TradeHistory> get tradeHistory => _tradeHistory;
+//   bool get isLoading => _isLoading;
+//   String? get errorMessage => _errorMessage;
+
+//   TradeHistoryProvider() {
+//     fetchTrades();
+//     // _connectWebSocket();
+//   }
+
+//   Future<void> fetchTrades() async {
+//     _isLoading = true;
+//     _errorMessage = null;
+//     notifyListeners();
+//     try {
+//       _tradeHistory = await _service.fetchTrades();
+//     } catch (e) {
+//       _errorMessage = e.toString();
+//     } finally {
+//       _isLoading = false;
+//       notifyListeners();
+//     }
+//   }
+
+//     void addTrade(OrderGet order) {
+//     final trade = TradeHistory(
+//       symbol: order.symbol,
+//       amount: order.amount,
+//       timestamp: DateTime.now().millisecondsSinceEpoch,
+  
+//     );
+//     _tradeHistory.add(trade);
+//     notifyListeners();
+//   }
+
+//   // void _connectWebSocket() {
+//   //   final channel = _service.connectWebSocket();
+
+//   //   _socketSubscription = channel.stream.listen(
+//   //     (message) {
+//   //       final data = json.decode(message);
+//   //       TradeHistory newOrder = TradeHistory.fromJson(data);
+
+//   //       _tradeHistory.insert(0, newOrder); 
+//   //       notifyListeners();
+//   //     },
+//   //     onError: (error) {
+//   //       _errorMessage = "WebSocket error: $error";
+//   //       notifyListeners();
+//   //     },
+//   //     onDone: () {
+//   //       _errorMessage = "WebSocket disconnected";
+//   //       notifyListeners();
+//   //     },
+//   //   );
+//   // }
+
+//   // @override
+//   // void dispose() {
+//   //   _socketSubscription.cancel();
+//   //   super.dispose();
+//   // }
+// }
+
+
+
+
 import 'dart:async';
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:olymp_trade/features/model/order_get_model.dart';
 import 'package:olymp_trade/features/model/trade_history_model.dart';
-import 'package:olymp_trade/services/trade_history_services.dart';
 
 class TradeHistoryProvider extends ChangeNotifier {
-  final TradeHistoryServices _service = TradeHistoryServices();
   List<TradeHistory> _tradeHistory = [];
   bool _isLoading = false;
   String? _errorMessage;
-  // late final StreamSubscription _socketSubscription;
 
   List<TradeHistory> get tradeHistory => _tradeHistory;
   bool get isLoading => _isLoading;
   String? get errorMessage => _errorMessage;
 
-  TradeHistoryProvider() {
-    fetchTrades();
-    // _connectWebSocket();
-  }
-
-  Future<void> fetchTrades() async {
-    _isLoading = true;
-    _errorMessage = null;
-    notifyListeners();
+  void addOrderToHistory(TradeHistory tradeHistory) {
     try {
-      _tradeHistory = await _service.fetchTrades();
-    } catch (e) {
-      _errorMessage = e.toString();
-    } finally {
-      _isLoading = false;
+      print("Adding trade: $tradeHistory");
+      _tradeHistory.add(tradeHistory);
+      print("Current trade history: $_tradeHistory");
+      notifyListeners();
+    } catch (error) {
+      _errorMessage = 'Failed to add order to history: $error';
+      print(_errorMessage);
       notifyListeners();
     }
   }
 
-    void addTrade(OrderGet order) {
-    final trade = TradeHistory(
-      symbol: order.symbol,
-      amount: order.amount,
-      timestamp: DateTime.now().millisecondsSinceEpoch,
-  
-    );
-    _tradeHistory.add(trade);
+  void clearTradeHistory() {
+    _tradeHistory.clear();
     notifyListeners();
   }
 
-  // void _connectWebSocket() {
-  //   final channel = _service.connectWebSocket();
+  Future<void> fetchTradeHistory() async {
+    try {
+      _isLoading = true;
+      notifyListeners();
 
-  //   _socketSubscription = channel.stream.listen(
-  //     (message) {
-  //       final data = json.decode(message);
-  //       TradeHistory newOrder = TradeHistory.fromJson(data);
+      await Future.delayed(Duration(seconds: 2));
 
-  //       _tradeHistory.insert(0, newOrder); 
-  //       notifyListeners();
-  //     },
-  //     onError: (error) {
-  //       _errorMessage = "WebSocket error: $error";
-  //       notifyListeners();
-  //     },
-  //     onDone: () {
-  //       _errorMessage = "WebSocket disconnected";
-  //       notifyListeners();
-  //     },
-  //   );
-  // }
-
-  // @override
-  // void dispose() {
-  //   _socketSubscription.cancel();
-  //   super.dispose();
-  // }
+      _isLoading = false;
+      notifyListeners();
+    } catch (error) {
+      _isLoading = false;
+      _errorMessage = 'Error fetching trade history: $error';
+      notifyListeners();
+    }
+  }
 }
-
-
-
-
 
