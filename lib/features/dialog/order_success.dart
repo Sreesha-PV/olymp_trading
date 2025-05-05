5b11bc1d551c790d429c8124e45675d4f2693a74
Sreesha-PV <sreeshaaapv2001@gmail.com>
2025-05-05 15:27:53 +0400

    Add files via upload

---
+++ lib/features/dialog/order_success.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void orderSuccessDialog(BuildContext context){
+  showDialog(
+    context: context, 
+    builder: (ctx){
+      return AlertDialog(
+        alignment: Alignment.center,
+        content: const Text(
+          'Order placed successfully',
+          style: TextStyle(color: Colors.green,fontSize: 16,fontWeight: FontWeight.bold),),
+          actions: [
+            GestureDetector(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: Container(
+              height: 54,
+              width: MediaQuery.of(context).size.width / 4,
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                gradient: const LinearGradient(
+                  colors: [
+                    Color.fromARGB(255, 76, 238, 238),
+                    Color.fromARGB(255, 74, 231, 43),
+                  ],
+                ),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: const Text(
+                'Ok',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            )
+          ],
+      );
+    });
+}
+
