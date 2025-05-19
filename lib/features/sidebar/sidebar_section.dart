34d81de4f4a10467aafd9280a39975a2893c4cb5
Sreesha-PV <sreeshaaapv2001@gmail.com>
2025-05-19 18:00:53 +0400

    Update sidebar_section.dart

--- lib/features/sidebar/sidebar_section.dart
+++ lib/features/sidebar/sidebar_section.dart
@@ -1,3 +1,222 @@
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:olymp_trade/features/drawers/sidebar_drawer/events_drawer.dart';
+// import 'package:olymp_trade/features/drawers/sidebar_drawer/help_drawer.dart';
+// import 'package:olymp_trade/features/drawers/sidebar_drawer/market_drawer.dart';
+// import 'package:olymp_trade/features/drawers/sidebar_drawer/trades_drawer.dart';
+// import 'package:badges/badges.dart' as badges;
+// import 'package:olymp_trade/features/provider/trade_badge_provider.dart';
+// import 'package:provider/provider.dart';
+
+// class Sidebar extends StatelessWidget {
+//   final Function(Widget) onItemSelected;
+
+//   const Sidebar({super.key, required this.onItemSelected});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     bool isMobile = MediaQuery.of(context).size.width < 600;
+//     return isMobile ? _buildBottomSidebar(context) : _buildVerticalSidebar();
+//   }
+
+//   Widget _buildVerticalSidebar() {
+//     final List<Map<String, dynamic>> sidebarItems = [
+//       {
+//         "icon": CupertinoIcons.arrow_down_right_arrow_up_left,
+//         "label": "Trades",
+//         "drawer": const TradesDrawer(),
+//       },
+//       {
+//         "icon": Icons.shopping_bag_outlined,
+//         "label": "Market",
+//         "drawer": const MarketDrawer(),
+//       },
+//       {
+//         "icon": Icons.emoji_events_outlined,
+//         "label": "Events",
+//         "drawer": const EventsDrawer(),
+//       },
+//       {
+//         "icon": Icons.help_outline,
+//         "label": "Help",
+//         "drawer": const HelpDrawer(),
+//       },
+//     ];
+
+//     return Container(
+//       width: 80,
+//       color: const Color.fromARGB(246, 17, 17, 17),
+//       child: Column(
+//         mainAxisAlignment: MainAxisAlignment.start,
+//         children: [
+//           const SizedBox(height: 90),
+//           ...sidebarItems.map((item) {
+           
+
+//               return Padding(
+//               padding: const EdgeInsets.only(top: 40),
+
+              
+//               child: SidebarItem(
+//                 icon: item['icon'],
+//                 label: item['label'],
+//                 onTap: () => onItemSelected(item['drawer']),
+//               ),
+//             );
+//           }).toList(),
+//         ],
+//       ),
+//     );
+//   }
+
+//   Widget _buildBottomSidebar(BuildContext context) {
+//     final List<Map<String, dynamic>> sidebarItems = [
+//       {
+//         "icon": CupertinoIcons.arrow_down_right_arrow_up_left,
+//         "label": "Trades",
+//         "index": 0,
+//       },
+//       {
+//         "icon": Icons.shopping_bag_outlined,
+//         "label": "Market",
+//         "index": 1,
+//       },
+//       {
+//         "icon": Icons.emoji_events_outlined,
+//         "label": "Events",
+//         "index": 2,
+//       },
+//       {
+//         "icon": Icons.help_outline,
+//         "label": "Help",
+//         "index": 3,
+//       },
+//     ];
+//     return Container(
+//       height: 70,
+//       color: const Color.fromARGB(246, 17, 17, 17),
+//       child: Row(
+//         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//         children: sidebarItems.map((item) {
+//           if(item['label']=='Trades'){
+//             return Consumer<TradeBadgeProvider>(
+//               builder:(context,badgeProvider,_){
+//                   return badges.Badge(
+//               showBadge: badgeProvider.hasNewTrade,
+//               position: badges.BadgePosition.topEnd(top: -8, end: -8),
+//               badgeStyle: const badges.BadgeStyle(
+//                 badgeColor: Colors.red),
+//               // badgeContent: const SizedBox(width: 6, height: 6),
+//               badgeContent: const Text('1'),
+//               child: SidebarItem(
+//                 icon: item['icon'],
+//                 label: item['label'],
+//                 onTap: () {
+//                 if (item['label'] == 'Trades') {
+//                   Provider.of<TradeBadgeProvider>(context, listen: false).clearBadge();
+//                 }
+//                 _openBottomDrawer(context, item['index']);
+//                 }
+//                 ),
+//               );
+//               });
+//           }else{
+//             return SidebarItem(
+//               icon: item['icon'], 
+//               label: item['label'],
+//               // onTap: () => onItemSelected(item['drawer']),
+//               onTap: () {
+//               if (item['label'] == 'Trades') {
+//                 Provider.of<TradeBadgeProvider>(context, listen: false).clearBadge();
+//               }
+//              onItemSelected(item['drawer']);
+//             }
+//            );
+              
+//           } 
+       
+//         }).toList(),
+//       ),
+//     );
+//   }
+//   void _openBottomDrawer(BuildContext context, int index) {
+//     showModalBottomSheet(
+//       context: context,
+//       isScrollControlled: true,
+//       backgroundColor: Colors.transparent,
+//       builder: (BuildContext context) {
+//         return Container(
+//           width: double.infinity,
+//           color: const Color.fromARGB(255, 24, 23, 23),
+//           child: Column(
+//             children: [
+//               Expanded(child: _getDrawerContent(index)),
+//             ],
+//           ),
+//         );
+//       },
+//     );
+//   }
+
+//   Widget _getDrawerContent(int index) {
+//     switch (index) {
+//       case 0:
+//         return const TradesDrawer();
+//       case 1:
+//         return const MarketDrawer();
+//       case 2:
+//         return const EventsDrawer();
+//       case 3:
+//         return const HelpDrawer();
+//       default:
+//         return Container();
+//     }
+//   }
+// }
+
+// class SidebarItem extends StatelessWidget {
+//   final IconData icon;
+//   final String label;
+//   final VoidCallback? onTap;
+
+//   const SidebarItem({
+//     super.key,
+//     required this.icon,
+//     required this.label,
+//     this.onTap,
+//   });
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return GestureDetector(
+//       onTap: onTap,
+//       child: Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: [
+//           Icon(icon, color: const Color.fromARGB(255, 163, 185, 179), size: 24),
+//           Text(
+//             label,
+//             style: const TextStyle(
+//                 color: Color.fromARGB(255, 163, 185, 179), fontSize: 12),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:olymp_trade/features/drawers/sidebar_drawer/events_drawer.dart';
@@ -51,18 +270,36 @@ class Sidebar extends StatelessWidget {
         children: [
           const SizedBox(height: 90),
           ...sidebarItems.map((item) {
-           
-
+            if (item['label'] == 'Trades') {
+              return Consumer<TradeBadgeProvider>(
+                builder: (context, badgeProvider, _) {
+                  return badges.Badge(
+                    showBadge: badgeProvider.hasNewTrade,
+                    position: badges.BadgePosition.topEnd(top: -8, end: -8),
+                    badgeStyle: const badges.BadgeStyle(badgeColor: Colors.red),
+                    badgeContent: const Text('1'),
+                    child: SidebarItem(
+                      icon: item['icon'],
+                      label: item['label'],
+                      onTap: () {
+                        Provider.of<TradeBadgeProvider>(context, listen: false)
+                            .clearBadge();
+                        onItemSelected(item['drawer']);
+                      },
+                    ),
+                  );
+                },
+              );
+            } else {
               return Padding(
-              padding: const EdgeInsets.only(top: 40),
-
-              
-              child: SidebarItem(
-                icon: item['icon'],
-                label: item['label'],
-                onTap: () => onItemSelected(item['drawer']),
-              ),
-            );
+                padding: const EdgeInsets.only(top: 40),
+                child: SidebarItem(
+                  icon: item['icon'],
+                  label: item['label'],
+                  onTap: () => onItemSelected(item['drawer']),
+                ),
+              );
+            }
           }).toList(),
         ],
       ),
@@ -98,47 +335,45 @@ class Sidebar extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: sidebarItems.map((item) {
-          if(item['label']=='Trades'){
+          if (item['label'] == 'Trades') {
             return Consumer<TradeBadgeProvider>(
-              builder:(context,badgeProvider,_){
-                  return badges.Badge(
-              showBadge: badgeProvider.hasNewTrade,
-              position: badges.BadgePosition.topEnd(top: -8, end: -8),
-              badgeStyle: const badges.BadgeStyle(
-                badgeColor: Colors.red),
-              // badgeContent: const SizedBox(width: 6, height: 6),
-              badgeContent: const Text('1'),
-              child: SidebarItem(
+                builder: (context, badgeProvider, _) {
+              return badges.Badge(
+                showBadge: badgeProvider.hasNewTrade,
+                position: badges.BadgePosition.topEnd(top: -8, end: -8),
+                badgeStyle: const badges.BadgeStyle(badgeColor: Colors.red),
+              
+                badgeContent: const Text('1'),
+                child: SidebarItem(
+                    icon: item['icon'],
+                    label: item['label'],
+                    onTap: () {
+                      if (item['label'] == 'Trades') {
+                        Provider.of<TradeBadgeProvider>(context, listen: false)
+                            .clearBadge();
+                      }
+                      _openBottomDrawer(context, item['index']);
+                    }),
+              );
+            });
+          } else {
+            return SidebarItem(
                 icon: item['icon'],
                 label: item['label'],
+                // onTap: () => onItemSelected(item['drawer']),
                 onTap: () {
-                if (item['label'] == 'Trades') {
-                  Provider.of<TradeBadgeProvider>(context, listen: false).clearBadge();
-                }
-                _openBottomDrawer(context, item['index']);
-                }
-                ),
-              );
-              });
-          }else{
-            return SidebarItem(
-              icon: item['icon'], 
-              label: item['label'],
-              // onTap: () => onItemSelected(item['drawer']),
-              onTap: () {
-              if (item['label'] == 'Trades') {
-                Provider.of<TradeBadgeProvider>(context, listen: false).clearBadge();
-              }
-             onItemSelected(item['drawer']);
-            }
-           );
-              
-          } 
-       
+                  if (item['label'] == 'Trades') {
+                    Provider.of<TradeBadgeProvider>(context, listen: false)
+                        .clearBadge();
+                  }
+                  onItemSelected(item['drawer']);
+                });
+          }
         }).toList(),
       ),
     );
   }
+
   void _openBottomDrawer(BuildContext context, int index) {
     showModalBottomSheet(
       context: context,
@@ -158,6 +393,7 @@ class Sidebar extends StatelessWidget {
     );
   }
 
+//
   Widget _getDrawerContent(int index) {
     switch (index) {
       case 0:
@@ -204,4 +440,3 @@ class SidebarItem extends StatelessWidget {
     );
   }
 }
-
