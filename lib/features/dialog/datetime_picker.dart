e6bba74f456eeeb6ff905a773e53ec67a1d17eda
Sreesha-PV <sreeshaaapv2001@gmail.com>
2025-05-27 17:55:41 +0400

    Update datetime_picker.dart

--- lib/features/dialog/datetime_picker.dart
+++ lib/features/dialog/datetime_picker.dart
@@ -1,250 +1,3 @@
-// import 'package:flutter/cupertino.dart';
-// import 'package:flutter/material.dart';
-
-// class DateTimePickerDemo extends StatefulWidget {
-//   const DateTimePickerDemo({super.key});
-
-//   @override
-//   // ignore: library_private_types_in_public_api
-//   _DateTimePickerDemoState createState() => _DateTimePickerDemoState();
-// }
-
-// class _DateTimePickerDemoState extends State<DateTimePickerDemo> {
-//   DateTime selectedDateTime = DateTime.now();
-
-//   void _showPicker(BuildContext context) {
-//     showModalBottomSheet(
-//       context: context,
-//       builder: (_) {
-//         return SizedBox(
-//           height: 250,
-//           child: CupertinoDatePicker(
-//             mode: CupertinoDatePickerMode.dateAndTime,
-//             initialDateTime: selectedDateTime,
-//             onDateTimeChanged: (DateTime newDateTime) {
-//               setState(() {
-//                 selectedDateTime = newDateTime;
-//               });
-//             },
-//             use24hFormat: true,
-//           ),
-//         );
-//       },
-//     );
-//   }  
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: const Text("Date & Time Picker")),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             Text(
-//               "Selected: ${selectedDateTime.toLocal()}",
-//               style: const TextStyle(fontSize: 16),
-//             ),
-//             const SizedBox(height: 20),
-//             ElevatedButton(
-//               onPressed: () => _showPicker(context),
-//               child: const Text("Pick Date & Time"),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-// import 'package:flutter/cupertino.dart';
-// import 'package:flutter/material.dart';
-// import 'package:olymp_trade/core/constants/app_colors.dart';
-
-// class TimeSelectionScreen extends StatefulWidget {
-//   const TimeSelectionScreen({super.key});
-
-//   @override
-//   State<TimeSelectionScreen> createState() => _TimeSelectionScreenState();
-// }
-
-// class _TimeSelectionScreenState extends State<TimeSelectionScreen> {
-//   String selectedDay = 'Today';
-//   TimeOfDay selectedTime = TimeOfDay.now();
-
-//   List<String> days = ['Today', 'Tomorrow'];
-//   @override
-//   Widget build(BuildContext context) {
-//     return Row(
-//       children: [
-//         // Row(
-//         //   children: [
-//             // _buildFilterChip('Any Profitability', width: 140),
-//             // const SizedBox(width: 10),
-//             // _buildFilterChip('from 70%', width: 100),
-//             // const SizedBox(width: 10),
-//             // _buildFilterChip('from 80%', width: 100),
-//         //   ],
-//         // ),
-//         Container(
-//           width: 100,
-//           color: Colors.grey[900],
-//           child: ListView.builder(
-//             itemCount: days.length,
-//             itemBuilder: (context, index) {
-//               String day = days[index];
-//               bool isSelected = selectedDay == day;
-
-//               return GestureDetector(
-//                 onTap: () {
-//                   setState(() {
-//                     selectedDay = day;
-//                   });
-//                 },
-//                 child: Container(
-//                   padding: const EdgeInsets.symmetric(vertical: 20),
-//                   color: isSelected ? Colors.greenAccent : Colors.transparent,
-//                   child: Center(
-//                     child: Text(
-//                       day,
-//                       style: TextStyle(
-//                         color: isSelected ? Colors.black : Colors.white,
-//                         fontWeight: FontWeight.bold,
-//                       ),
-//                     ),
-//                   ),
-//                 ),
-//               );
-//             },
-//           ),
-//         ),
-//         Expanded(
-//           child: Container(
-//             color: Colors.black,
-//             child: CupertinoDatePicker(
-//               mode: CupertinoDatePickerMode.time,
-//               use24hFormat: true,
-//               initialDateTime: _buildInitialDateTime(),
-//               onDateTimeChanged: (DateTime newTime) {
-//                 setState(() {
-//                   selectedTime = TimeOfDay.fromDateTime(newTime);
-//                 });
-//               },
-//             ),
-//           ),
-//         ),
-//       ],
-//     );
-//   }
-
-//   DateTime _buildInitialDateTime() {
-//     DateTime now = DateTime.now();
-//     if (selectedDay == 'Tomorrow') {
-//       return DateTime(now.year, now.month, now.day + 1, now.hour, now.minute);
-//     } else {
-//       return now;
-//     }
-//   }
-//   }
-
-
-
-
-// import 'package:flutter/cupertino.dart';
-// import 'package:flutter/material.dart';
-// import 'package:olymp_trade/core/constants/app_colors.dart';
-
-// class TimeSelectionScreen extends StatefulWidget {
-//   const TimeSelectionScreen({super.key});
-
-//   @override
-//   State<TimeSelectionScreen> createState() => _TimeSelectionScreenState();
-// }
-
-// class _TimeSelectionScreenState extends State<TimeSelectionScreen> {
-//   String selectedDay = 'Today';
-//   TimeOfDay selectedTime = TimeOfDay.now();
-//   DateTime selectedDateTime = DateTime.now();
-
-//   List<String> days = ['Today', 'Tomorrow'];
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//     backgroundColor: Colors.black,
-//     body: SafeArea(
-//       child: SingleChildScrollView(
-//         child: Padding(
-//           padding: const EdgeInsets.all(16.0),
-//           child: Column(
-//             children: [
-//               Row(
-//                 children: [
-//                   _buildFilterChip('Any Profitability', width: 140),
-//                     const SizedBox(width: 10),
-//                   _buildFilterChip('from 70%', width: 100),
-//                   const SizedBox(width: 10),
-//                   _buildFilterChip('from 80%', width: 100),
-//                   const SizedBox(height: 10,), 
-//                 ],
-//               ),
-
-//                SizedBox(
-//               height: 200,
-//               child: CupertinoDatePicker(
-//               mode: CupertinoDatePickerMode.dateAndTime,
-//               initialDateTime: selectedDateTime,
-//               onDateTimeChanged: (DateTime newDateTime){
-//                 setState(() {
-//                   selectedDateTime = newDateTime;
-//                 });
-//               },
-//               use24hFormat: true,
-//               ),
-//         ),
-//            ],
-//           ),
-//         ),
-//       ),
-//     ),
-//   );
-//   }
-
-//   Widget _buildFilterChip(String text, {required double width}) {
-//     return Container(
-//       width: width,
-//       height: 40,
-//       decoration: BoxDecoration(
-//         color: AppColors.cardBackground,
-//         borderRadius: BorderRadius.circular(20),
-//       ),
-//       child: Padding(
-//         padding: const EdgeInsets.only(left: 10),
-//         child: Align(
-//           alignment: Alignment.centerLeft,
-//           child: Text(
-//             text,
-//             style: const TextStyle(fontSize: 15, color: AppColors.textColor),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-
-//   DateTime _buildInitialDateTime() {
-//     DateTime now = DateTime.now();
-//     if (selectedDay == 'Tomorrow') {
-//       return DateTime(now.year, now.month, now.day + 1, now.hour, now.minute);
-//     } else {
-//       return now;
-//     }
-//   }
-//   }
-
-
-
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:olymp_trade/core/constants/app_colors.dart';
@@ -367,75 +120,190 @@ class _TimeSelectionScreenState extends State<TimeSelectionScreen> {
 
 
 
-void _showTimePickerSheet() {
-  showModalBottomSheet(
-    context: context,
-    backgroundColor: Colors.black,
-    shape: const RoundedRectangleBorder(
-      borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
-    ),
-    builder: (_) {
-      return Container(
-        height: 300,
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            Expanded(
-              child: Row(
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:olymp_trade/core/constants/app_colors.dart';
+import 'package:olymp_trade/features/provider/tradesettings_provider.dart';
+import 'package:provider/provider.dart';
+
+class TimeSelectionScreen extends StatefulWidget {
+  const TimeSelectionScreen({super.key});
+
+  @override
+  State<TimeSelectionScreen> createState() => _TimeSelectionScreenState();
+}
+
+class _TimeSelectionScreenState extends State<TimeSelectionScreen> {
+  String selectedDay = 'Today';
+  TimeOfDay selectedTime = TimeOfDay.now();
+
+  List<String> days = ['Today', 'Tomorrow'];
+  final TextEditingController _controller = TextEditingController();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        // child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Row(
                 children: [
-                  // Day picker (Today, Tomorrow)
-                  Expanded(
+                  _buildFilterChip('Any Profitability', width: 140),
+                  const SizedBox(width: 10),
+                  _buildFilterChip('from 70%', width: 100),
+                  const SizedBox(width: 10),
+                  _buildFilterChip('from 80%', width: 100),
+                ],
+              ),
+              const SizedBox(height: 20),
+              
+
+              Row(
+                children: [
+                  // Day Picker
+                  SizedBox(
+                    width: 120,
+                    height: 200,
                     child: CupertinoPicker(
-                      backgroundColor: Colors.black,
-                      itemExtent: 32,
-                      scrollController: FixedExtentScrollController(
-                          initialItem: days.indexOf(selectedDay)),
-                      onSelectedItemChanged: (index) {
-                        setState(() {
-                          selectedDay = days[index];
-                        });
-                      },
-                      children: days
-                          .map((day) => Center(
-                                child: Text(day,
-                                    style: const TextStyle(color: Colors.white)),
-                              ))
-                          .toList(),
+                    backgroundColor: Colors.transparent, 
+                    itemExtent: 32,
+                    scrollController: FixedExtentScrollController(
+                      initialItem: days.indexOf(selectedDay),
                     ),
+                    onSelectedItemChanged: (index) {
+                      setState(() {
+                        selectedDay = days[index];
+                      });
+                    },
+                    children: days.map((day) {
+                      return Center(
+                        child: Text(
+                          day,
+                          style: const TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                  )
+
+                    // child: CupertinoPicker(
+                    //   backgroundColor: Colors.transparent,
+                    //   itemExtent: 32,
+                    //   scrollController: FixedExtentScrollController(
+                    //     initialItem: days.indexOf(selectedDay),
+                    //   ),
+                    //   onSelectedItemChanged: (index) {
+                    //     setState(() {
+                    //       selectedDay = days[index];
+                    //     });
+                    //   },
+                    //   children: days
+                    //       .map((day) => Center(
+                    //             child: Text(
+                    //               day,
+                    //               style: const TextStyle(color: Colors.white,fontWeight: FontWeight.bold),
+                    //             ),
+                    //           ))
+                    //       .toList(),
+                    // ),
                   ),
-                  // Time picker
+
+                  const SizedBox(width: 20),
+
+                  // Time Picker
                   Expanded(
-                    child: CupertinoDatePicker(
-                      mode: CupertinoDatePickerMode.time,
-                      use24hFormat: true,
-                      initialDateTime: _buildInitialDateTime(),
-                      onDateTimeChanged: (newDateTime) {
-                        setState(() {
-                          selectedTime =
-                              TimeOfDay.fromDateTime(newDateTime);
-                        });
-                      },
+                    child: SizedBox(
+                      height: 200,
+                      child: CupertinoDatePicker(
+                        mode: CupertinoDatePickerMode.time,
+                        use24hFormat: true,
+                        initialDateTime: _buildInitialDateTime(),
+                        onDateTimeChanged: (newDateTime) {
+                          setState(() {
+                            selectedTime = TimeOfDay.fromDateTime(newDateTime);
+                          });
+                        },
+                      ),
                     ),
                   ),
                 ],
               ),
-            ),
-            const SizedBox(height: 12),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.greenAccent,
-                foregroundColor: Colors.black,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10)),
+              const SizedBox(height: 20),
+              Text(
+                "Your trade will be opened today at ${selectedTime.format(context)}",
+                style: const TextStyle(color: AppColors.buttonDisabled, fontSize: 18,fontWeight: FontWeight.bold),
               ),
-              child: const Text('Save'),
-            ),
-          ],
+              const SizedBox(height: 10,),
+              Container(
+                height: 60,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  gradient: const LinearGradient(
+                    colors:[
+                      Color.fromARGB(255,76,238,238),
+                      Color.fromARGB(255, 74, 231, 43)
+                    ] 
+                  ),
+                  borderRadius: BorderRadius.circular(10)
+                ),
+                child: Align(
+                  alignment: Alignment.center,
+                  child: GestureDetector(
+                    onTap: () {
+                        final provider = Provider.of<TradeSettingsProvider>(context, listen: false);
+                      provider.setCustomLabels(
+                        "Order by time: Today at ${selectedTime.format(context)}",
+                        "Profit when ${_controller.text} hit",
+                      );
+
+                      Navigator.pop(context); 
+                    },
+                    child: const Text(
+                      'Save',
+                      style: TextStyle(color: AppColors.background,fontSize: 20,fontWeight: FontWeight.bold),),
+                  ))
+              )
+            ],
+          ),
         ),
-      );
-    },
-  );
+        // ),
+      ),
+    );
+  }
+  Widget _buildFilterChip(String text, {required double width}) {
+    return Container(
+      width: width,
+      height: 40,
+      decoration: BoxDecoration(
+        color: AppColors.cardBackground,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 10),
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: Text(
+            text,
+            style: const TextStyle(fontSize: 15, color: AppColors.textColor),
+          ),
+        ),
+      ),
+    );
+  }
+  DateTime _buildInitialDateTime() {
+    final now = DateTime.now();
+    final date = selectedDay == 'Tomorrow'
+        ? DateTime(now.year, now.month, now.day + 1)
+        : DateTime(now.year, now.month, now.day);
+    return DateTime(date.year, date.month, date.day, selectedTime.hour,
+        selectedTime.minute);
+ }
+
 }
